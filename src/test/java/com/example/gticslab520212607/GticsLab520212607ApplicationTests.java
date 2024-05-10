df963695df7bfd13f03fd7d698123874d4eaa027
Arkel <144744565+WalkedPlate@users.noreply.github.com>
2024-05-09 19:19:08 -0500

    Initial commit

---
+++ src/test/java/com/example/gticslab520212607/GticsLab520212607ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gticslab520212607;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab520212607ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
